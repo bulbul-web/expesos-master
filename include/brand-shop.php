4f288c16ca31c5ca8d6a5a6d7ce1dc6ecbc968d4
bulbul <sakarnirakar123@gmail.com>
2021-02-23 00:05:43 +0600

    brand-shop page added with all products and pagination

---
+++ include/brand-shop.php
@@ -0,0 +1,303 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="brand-header">
+                <div class="image-area">
+                    <img src="./assets/images/give_take_enter.png" class="img-responsive"/>
+                    
+                    <div class="content-area">
+                        <div class="shop-icon">
+                            <i class="fa fa-university" aria-hidden="true"></i>
+                        </div>
+                        <div class="company-info">
+                            <h4>Give & Take Enterprise</h4>
+                            <h6>156 Followers</h6>
+                            <p>89% Positive Seller Ratings</p>
+                        </div>
+                        <div class="chat-shop">
+                            <a href="#">
+                                <p class="icon"><i class="fa fa-commenting" aria-hidden="true"></i></p>
+                                <p class="text">Chat Now</p>
+                            </a>
+                        </div>
+                        <div class="follow">
+                            <a href="#">
+                                <p class="icon"><i class="fa fa-users" aria-hidden="true"></i></p>
+                                <p class="text">FOLLOW</p>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div class="menu-shop">
+                        <ul>
+                            <li><a href="brand-shop.php" style="color: #ef9b0f;">All Products</a></li>
+                            <li><a href="#profile">Profile</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-12">
+            <div class="shop-product-area" id="shopProductArea">
+                <div class="row">
+                    <div class="col-lg-2 col-md-2 col-sm-3 col-4">
+                        <a href="product-details.php" class="swiper-a-link">
+                            <div class="post-item">
+                                <div class="post-thumb">
+                                    <img src="assets/images/feature/21.jpg" alt="Electronic">
+                                <div class="post-wish">
+
+                                </div>
+                                <div class="post-cart">
+                                    <div class="product-action-link-front">
+                                        <a class="view-modal link" href="product-details.php"><i class="fa fa-eye"></i></a>
+                                        <a class="link" href="#"><i class="fa fa-heart"></i></a>
+                                        <a class="link" href="cart.php"><i class="fa fa-cart-plus"></i></a>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="post-text">  
+                                    <h6>charecter will be maximum 32</h6>
+                                    <div class="price-tag">
+                                        <div class="row">
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-now">$659.00</p>
+                                            </div>
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-old">-$759.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-3 col-4">
+                        <a href="product-details.php" class="swiper-a-link">
+                            <div class="post-item">
+                                <div class="post-thumb">
+                                    <img src="assets/images/feature/20.jpg" alt="Electronic">
+                                <div class="post-wish">
+
+                                </div>
+                                <div class="post-cart">
+                                    <div class="product-action-link-front">
+                                        <a class="view-modal link" href="product-details.php"><i class="fa fa-eye"></i></a>
+                                        <a class="link" href="#"><i class="fa fa-heart"></i></a>
+                                        <a class="link" href="cart.php"><i class="fa fa-cart-plus"></i></a>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="post-text">  
+                                    <h6>charecter will be maximum 32</h6>
+                                    <div class="price-tag">
+                                        <div class="row">
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-now">$659.00</p>
+                                            </div>
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-old">-$759.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-3 col-4">
+                        <a href="product-details.php" class="swiper-a-link">
+                            <div class="post-item">
+                                <div class="post-thumb">
+                                    <img src="assets/images/feature/22.jpg" alt="Electronic">
+                                <div class="post-wish">
+
+                                </div>
+                                <div class="post-cart">
+                                    <div class="product-action-link-front">
+                                        <a class="view-modal link" href="product-details.php"><i class="fa fa-eye"></i></a>
+                                        <a class="link" href="#"><i class="fa fa-heart"></i></a>
+                                        <a class="link" href="cart.php"><i class="fa fa-cart-plus"></i></a>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="post-text">  
+                                    <h6>charecter will be maximum 32</h6>
+                                    <div class="price-tag">
+                                        <div class="row">
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-now">$659.00</p>
+                                            </div>
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-old">-$759.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-3 col-4">
+                        <a href="product-details.php" class="swiper-a-link">
+                            <div class="post-item">
+                                <div class="post-thumb">
+                                    <img src="assets/images/feature/23.jpg" alt="Electronic">
+                                <div class="post-wish">
+
+                                </div>
+                                <div class="post-cart">
+                                    <div class="product-action-link-front">
+                                        <a class="view-modal link" href="product-details.php"><i class="fa fa-eye"></i></a>
+                                        <a class="link" href="#"><i class="fa fa-heart"></i></a>
+                                        <a class="link" href="cart.php"><i class="fa fa-cart-plus"></i></a>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="post-text">  
+                                    <h6>charecter will be maximum 32</h6>
+                                    <div class="price-tag">
+                                        <div class="row">
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-now">$659.00</p>
+                                            </div>
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-old">-$759.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-3 col-4">
+                        <a href="product-details.php" class="swiper-a-link">
+                            <div class="post-item">
+                                <div class="post-thumb">
+                                    <img src="assets/images/feature/24.jpg" alt="Electronic">
+                                <div class="post-wish">
+
+                                </div>
+                                <div class="post-cart">
+                                    <div class="product-action-link-front">
+                                        <a class="view-modal link" href="product-details.php"><i class="fa fa-eye"></i></a>
+                                        <a class="link" href="#"><i class="fa fa-heart"></i></a>
+                                        <a class="link" href="cart.php"><i class="fa fa-cart-plus"></i></a>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="post-text">  
+                                    <h6>charecter will be maximum 32</h6>
+                                    <div class="price-tag">
+                                        <div class="row">
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-now">$659.00</p>
+                                            </div>
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-old">-$759.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-3 col-4">
+                        <a href="product-details.php" class="swiper-a-link">
+                            <div class="post-item">
+                                <div class="post-thumb">
+                                    <img src="assets/images/feature/25.jpg" alt="Electronic">
+                                <div class="post-wish">
+
+                                </div>
+                                <div class="post-cart">
+                                    <div class="product-action-link-front">
+                                        <a class="view-modal link" href="product-details.php"><i class="fa fa-eye"></i></a>
+                                        <a class="link" href="#"><i class="fa fa-heart"></i></a>
+                                        <a class="link" href="cart.php"><i class="fa fa-cart-plus"></i></a>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="post-text">  
+                                    <h6>charecter will be maximum 32</h6>
+                                    <div class="price-tag">
+                                        <div class="row">
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-now">$659.00</p>
+                                            </div>
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-old">-$759.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-3 col-4">
+                        <a href="product-details.php" class="swiper-a-link">
+                            <div class="post-item">
+                                <div class="post-thumb">
+                                    <img src="assets/images/feature/27.jpg" alt="Electronic">
+                                <div class="post-wish">
+
+                                </div>
+                                <div class="post-cart">
+                                    <div class="product-action-link-front">
+                                        <a class="view-modal link" href="product-details.php"><i class="fa fa-eye"></i></a>
+                                        <a class="link" href="#"><i class="fa fa-heart"></i></a>
+                                        <a class="link" href="cart.php"><i class="fa fa-cart-plus"></i></a>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="post-text">  
+                                    <h6>charecter will be maximum 32</h6>
+                                    <div class="price-tag">
+                                        <div class="row">
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-now">$659.00</p>
+                                            </div>
+                                            <div class="col-md-6 col-6">
+                                                <p class="price-old">-$759.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="shop-page">
+                            <div class="pagination-area  d-flex flex-wrap justify-content-center">
+                                <ul class="pagination  d-flex flex-wrap m-0">
+                                      <li class="prev">
+                                        <a href="#"><i class="fa fa-angle-double-left"></i></a>
+                                      </li>
+                                      <li><a href="#">1</a></li>
+                                      <li><a href="#" class="active d-none d-md-block">2</a></li>
+                                      <li><a href="#" class="d-none d-md-block">3</a></li>
+                                      <li class="dot">....</li>
+                                      <li><a href="#" class="d-none d-md-block">4</a></li>
+                                      <li class="next">
+                                        <a href="#"><i class="fa fa-angle-double-right"></i></a>
+                                      </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div id="profile">
+                <p>this is profile</p>
+            </div>
+        </div>
+    </div>
+</div>
